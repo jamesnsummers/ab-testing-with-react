930337cdb3161be0952d5f9433f203fbdd2832b9
James Summers <227wLmrlcY0a>
2018-05-11 11:12:26 -0500

    removed comment

--- public/test-code.js
+++ public/test-code.js
@@ -1,7 +1,5 @@
 // this will persist through all state changes
-console.log('TEST-CODE CONNECTED');
 window.clearheadHook = function() {
   let headerElem = document.querySelector('h1');
-  // this will persist through all state changes
   headerElem.style.color = '#fff';
 };
